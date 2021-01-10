b6d6a76c6f74473251b28b226482600ca4ce1901
Furkan Öztürk <fozturk@iperasolutions.com>
2021-01-10 21:53:37 +0300

    add UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::query()->create([
+            'name' => 'furkan öztürk',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
