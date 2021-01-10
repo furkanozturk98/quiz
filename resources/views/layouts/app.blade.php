1348a4a5996b2d8cc555fc1735ecbe3ee551cf44
Furkan Öztürk <fozturk@iperasolutions.com>
2021-01-10 21:53:07 +0300

    refactor: prepare layout, install bootstrap, add user seeder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{$header}} - {{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -27,14 +27,18 @@
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                        {{ __('Dashboard') }}
+                    </h2>
                 </div>
             </header>
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                <div class="py-12">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        {{ $slot }}
+                    </div>
+                </div>
         </div>
 
         @stack('modals')
