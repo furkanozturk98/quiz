b19a047ac95d33c407c63b6b7c7bee21f12efc8a
Furkan Öztürk <fozturk@iperasolutions.com>
2021-01-12 03:42:52 +0300

    feat(question): add question controller

---
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Question;
+use App\Models\Quiz;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Quiz $quiz
+     * @return View
+     */
+    public function index(Quiz $quiz)
+    {
+        return view('admin.question.index', [
+            'quiz' => $quiz
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param Quiz $quiz
+     * @return View
+     */
+    public function create(Quiz $quiz)
+    {
+        return view('admin.question.form', [
+            'quiz' => $quiz,
+            'question' => new Question()
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Quiz $quiz
+     * @param Question $question
+     * @return void
+     */
+    public function show(Quiz $quiz, Question $question)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Quiz $quiz
+     * @param Question $question
+     * @return View
+     */
+    public function edit(Quiz $quiz,Question $question)
+    {
+        return view('admin.question.form', [
+            'quiz' => $quiz,
+            'question' => $question
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param Quiz $quiz
+     * @param int $id
+     * @return void
+     */
+    public function update(Request $request, Quiz $quiz ,$id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Quiz $quiz
+     * @param Question $question
+     * @return void
+     */
+    public function destroy(Quiz $quiz,Question $question)
+    {
+        //
+    }
+}
